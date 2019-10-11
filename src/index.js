a37b9e18ef507f829cd6093cbfa7476300b6696b
dgm <vsl_sv@bk.ru>
2019-10-11 12:18:17 +0300

    feat: add encodind one letter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -38,13 +39,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    console.log(expr);
-    console.log('\n' + "---------" + "\n");
-    console.log(expr.slice(-10)); //откусываем кусок строки
-    //TODO добавить преобразование буквы
-    
-    
-    
+    // console.log(expr);
+//    console.log('\n' + "---------" + "\n");
+    console.log(expr.slice(-10)); // откусываем кусок строки
+    // TODO добавить преобразование буквы
+
+    let rawSymbol = expr.slice(-10); // отрезок строки, в котором одна буква
+    let symbol = '';
+
+    for (let index = 0; index < 10; index = index + 2) {  // переводим двоичный код в строку
+        var element = rawSymbol[index] + rawSymbol[index+1];
+        switch (element) {
+            case "00":
+                break;
+            case "11":
+                symbol = symbol + "-";
+                break;
+            case "10":
+                symbol = symbol + ".";
+                break;
+            default:
+                break;
+        }
+    }
+    console.log(rawSymbol + " ноли и единицы " + symbol + " точки и тире " );
+    //        console.log();
+    console.log(MORSE_TABLE[symbol] + " тут один символ");
+        
+                
 }
 
 module.exports = {
