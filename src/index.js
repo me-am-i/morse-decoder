b76ada521621dd138e159bb514d635301f0c46dc
dgm <vsl_sv@bk.ru>
2019-10-11 17:28:57 +0300

    refactor: remove old code and comments

--- src/index.js
+++ src/index.js
@@ -39,36 +39,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-  //  console.log(expr);
-  //  console.log('\n' + "---------" + "\n");
-//    console.log(expr.slice(-10)); // откусываем кусок строки
-    // TODO добавить преобразование буквы
-
-   
-   // let rawSymbol = expr.slice(-20, expr.length);
-   // console.log("нач: " + rawSymbol + " .кон");
-
-   // let rawSymbol = expr.slice(-200000, -200010);
-    //console.log("нач: " + typeof rawSymbol + " " + rawSymbol.length + " .кон");
-
-
-
     let endPoint = expr.length;    
     let beginPoint = endPoint - 10;
     let result = "";
        
     do {
-        let rawSymbol = expr.slice(beginPoint, endPoint);
+        let rawSymbol = expr.slice(beginPoint, endPoint); // откусываем кусок строки
         beginPoint = beginPoint - 10;
         endPoint = endPoint - 10;
-   //     console.log("нач: " + typeof rawSymbol + " " + rawSymbol.length + " .кон");
-        
+          
         let symbol = '';
 
         if (rawSymbol == "**********") {
             MORSE_TABLE[symbol] = " ";
         }
-        else {for (let index = 0; index < 10; index = index + 2) {  // переводим двоичный код в строку
+        else {
+            for (let index = 0; index < 10; index = index + 2) {  // переводим двоичный код в строку
             var element = rawSymbol[index] + rawSymbol[index+1];
                 switch (element) {
                     case "00":
@@ -83,52 +69,12 @@ function decode(expr) {
                         break;
                     }
                 }
-            }      
-                
-           //     console.log(rawSymbol + " ноли и единицы " + symbol + " точки и тире " );
-    //        console.log();
-         //       console.log(MORSE_TABLE[symbol] + " тут один символ");
-                result = MORSE_TABLE[symbol] + result;
+            }
+        result = MORSE_TABLE[symbol] + result;
+        
+    } while (expr.slice(beginPoint, endPoint).length != 0); // проверяем, что осталось
 
-    } while (expr.slice(beginPoint, endPoint).length != 0);
-
-//console.log(result + " - итог");
-
-return result;
-    
-// todo добавить проверку на пробелы
-    
-
-    
-
-
-
-    // этот кусок работает (ниже)
-if (0) {
-
-    let rawSymbol = expr.slice(-10); // отрезок строки, в котором одна буква
-    let symbol = '';
-
-    for (let index = 0; index < 10; index = index + 2) {  // переводим двоичный код в строку
-        var element = rawSymbol[index] + rawSymbol[index+1];
-        switch (element) {
-            case "00":
-                break;
-            case "11":
-                symbol = symbol + "-";
-                break;
-            case "10":
-                symbol = symbol + ".";
-                break;
-            default:
-                break;
-        }
-    }
-    console.log(rawSymbol + " ноли и единицы " + symbol + " точки и тире " );
-    //        console.log();
-    console.log(MORSE_TABLE[symbol] + " тут один символ");
-  }      
-                
+return result;               
 }
 
 module.exports = {
